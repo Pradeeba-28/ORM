70961a676c62e4938c25833816754349e5835809
Pradeeba-28 <pradeeba728@gmail.com>
2026-05-09 18:28:18 +0530

    Added Django ORM FoodDelivery project

---
+++ exp1/foodapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import FoodDelivery_DB, FoodDelivery_DBAdmin
+
+admin.site.register(FoodDelivery_DB, FoodDelivery_DBAdmin)
---
+++ exp1/foodapp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib import admin
+
+class FoodDelivery_DB(models.Model):
+    Order_ID = models.IntegerField(primary_key=True)
+    CustomerName = models.CharField(max_length=30)
+    OrderDate = models.DateField()
+    ItemName = models.CharField(max_length=100)
+    OrderQty = models.IntegerField()
+    UnitPrice = models.FloatField()
+    TotalAmount = models.FloatField()
+    DeliveryAddress = models.CharField(max_length=200)
+
+class FoodDelivery_DBAdmin(admin.ModelAdmin):
+    list_display = (
+        'Order_ID',
+        'CustomerName',
+        'OrderDate',
+        'ItemName',
+        'OrderQty',
+        'UnitPrice',
+        'TotalAmount',
+        'DeliveryAddress'
+    )
